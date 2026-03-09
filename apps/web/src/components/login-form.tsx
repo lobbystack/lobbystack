d1deda5ee49a8555a0af0738d8b2e6f4c031ad6c
morencyr <raphaelmorency@gmail.com>
2026-03-09 18:39:29 -0400

    OPE-37 adjust login button height

--- apps/web/src/components/login-form.tsx
+++ apps/web/src/components/login-form.tsx
@@ -75,7 +75,7 @@ export function LoginForm({
           </Field>
 
           <Field>
-            <Button disabled={isSubmitting} type="submit">
+            <Button className="h-12" disabled={isSubmitting} type="submit">
               {isSubmitting ? "Signing in..." : "Sign in"}
             </Button>
           </Field>
